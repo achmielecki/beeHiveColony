3fea7db120fe96a3b367fc392aae6aae28cbaa2a
Aleksandra <maciejowska.aleksandra@gmail.com>
2024-01-24 00:12:12 +0100

    Dodanie panelu bocznego

--- hiveGUI.py
+++ hiveGUI.py
@@ -1,4 +1,3 @@
-import time
 import pygame
 from beehive.bee.artificialBeeColonyBehaviour import Role, bee_nectar_max_carry
 
@@ -6,19 +5,26 @@ from beehive.bee.artificialBeeColonyBehaviour import Role, bee_nectar_max_carry
 class HiveGUI:
     def __init__(self, world):
         self.world = world
-        self.width = 1000  # 1280
-        self.height = 1000  # 720
-        self.ratio = self.width / world.get_size()
+        self.width = 1400
+        self.height = 1000
+        self.simulation_dimension = 1000  # wymiary symulowanego świata to 1000x1000 px
+        self.ratio = self.simulation_dimension / world.get_size()
         self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption("BeeHiveColony")
-        # self.bee_img = pygame.image.load('assets/bee_left1.png')
+
         self.grass_img = pygame.image.load('assets/grass.png')
+        self.honeycomb_img = pygame.image.load('assets/honeycomb.png')
         self.bee_img_blue = pygame.image.load('assets/bee1_left_blue.png')
         self.bee_img_orange = pygame.image.load('assets/bee1_left_orange.png')
         self.bee_img_red = pygame.image.load('assets/bee1_left_red.png')
         self.hive_img = pygame.image.load('assets/bee_hive.png')
         self.bush_img = pygame.image.load('assets/bush.png')
         self.bush_flower_img = pygame.image.load('assets/bush_flowers.png')
+
+        pygame.display.set_caption("Wizualne modelowanie ula pszczelego")
+        pygame.display.set_icon(self.bee_img_orange)
+
+        pygame.init()
+        self.font = pygame.font.SysFont('Comic Sans MS', 22)
 
     def run(self):
         running = True
@@ -28,7 +34,7 @@ class HiveGUI:
                 if event.type == pygame.QUIT:
                     running = False
 
-            # self.screen.fill((255, 255, 255))
+            self.screen.fill((226, 177, 86))
             self.screen.blit(self.grass_img, (0, 0))
             self.draw_world()
 
@@ -40,45 +46,114 @@ class HiveGUI:
         self.draw_food_sources(self.world.get_food_sources())
         self.draw_hives(self.world.get_hives())
 
+        self.draw_side_panel(self.world.get_hives()[0], self.world)
+
     def draw_hives(self, hives):
         for hive in hives:
             self.draw_hive_bees(hive)
-            # pygame.draw.rect(self.screen, (255, 0, 0), (self.scale(hive.get_x()), self.scale(hive.get_y()), 5, 5))
             hive_rect = self.hive_img.get_rect(center=(self.scale(hive.get_x()), self.scale(hive.get_y())))
             self.screen.blit(self.hive_img, hive_rect)
 
     def draw_hive_bees(self, hive):
         for bee in hive.get_bees():
-            if bee.behaviour.role == Role.scout:
-                # pygame.draw.circle(self.screen, (204, 102, 0), (self.scale(bee.get_x()), self.scale(bee.get_y())), 3)
-                bee_rect = self.bee_img_orange.get_rect(center=(self.scale(bee.get_x()), self.scale(bee.get_y())))
-                self.screen.blit(self.bee_img_orange, bee_rect)
-            elif bee.behaviour.role == Role.employed_in_hive:
-                pass
-            elif bee.behaviour.is_dancing:
-                # pygame.draw.circle(self.screen, (255, 0, 0), (self.scale(bee.get_x()), self.scale(bee.get_y())), 3)
-                bee_rect = self.bee_img_red.get_rect(center=(self.scale(bee.get_x()), self.scale(bee.get_y())))
-                self.screen.blit(self.bee_img_red, bee_rect)
-            else:
-                # pygame.draw.circle(self.screen, (0, 0, 255), (self.scale(bee.get_x()), self.scale(bee.get_y())), 3)
-                bee_rect = self.bee_img_blue.get_rect(center=(self.scale(bee.get_x()), self.scale(bee.get_y())))
-                self.screen.blit(self.bee_img_blue, bee_rect)
+            if self.scale(bee.get_x()) < 1000:
+                if bee.behaviour.role == Role.scout:
+                    bee_rect = self.bee_img_orange.get_rect(center=(self.scale(bee.get_x()), self.scale(bee.get_y())))
+                    self.screen.blit(self.bee_img_orange, bee_rect)
+                elif bee.behaviour.role == Role.employed_in_hive:
+                    pass
+                elif bee.behaviour.is_dancing:
+                    pass
+                else:
+                    bee_rect = self.bee_img_blue.get_rect(center=(self.scale(bee.get_x()), self.scale(bee.get_y())))
+                    self.screen.blit(self.bee_img_blue, bee_rect)
 
     def draw_food_sources(self, food_sources):
         for food_source in food_sources:
             if food_source.discovered:
                 if food_source.current_amount >= bee_nectar_max_carry:
-                    # pygame.draw.rect(self.screen, (0, 255, 0),
-                    #                  (self.scale(food_source.get_x()), self.scale(food_source.get_y()), 5, 5))
                     bush_rect = self.bush_flower_img.get_rect(
                         center=(self.scale(food_source.get_x()), self.scale(food_source.get_y())))
                     self.screen.blit(self.bush_flower_img, bush_rect)
                 else:
-                    # pygame.draw.rect(self.screen, (255, 165, 0),
-                    #                  (self.scale(food_source.get_x()), self.scale(food_source.get_y()), 5, 5))
                     bush_rect = self.bush_img.get_rect(
                         center=(self.scale(food_source.get_x()), self.scale(food_source.get_y())))
                     self.screen.blit(self.bush_img, bush_rect)
 
     def scale(self, x):
         return self.ratio * x
+
+    def draw_side_panel(self, hive, world):
+        time = world.get_time()
+
+        # tło panelu bocznego
+        self.screen.blit(self.honeycomb_img, (self.simulation_dimension, 0))
+
+        # czas
+        day_text = self.font.render('DZIEŃ ' + str(time.day), True, (0, 0, 0))
+        self.screen.blit(day_text, (self.width - 380, 30))
+
+        minutes = ('0' + str(time.minute)) if (time.minute < 10) else str(time.minute)
+        time_text = self.font.render(str(time.hour) + ':' + minutes, True, (0, 0, 0))
+        self.screen.blit(time_text, (self.width - 80, 30))
+
+        # pogoda
+        temperature_text = self.font.render('Temperatura: ' + '--' + ' °C', True, (0, 0, 0))
+        self.screen.blit(temperature_text, (self.width - 380, 100))
+
+        humidity_text = self.font.render('Wilgotność: ' + '--' + '%', True, (0, 0, 0))
+        self.screen.blit(humidity_text, (self.width - 380, 150))
+
+        # statystyki ula
+        statistics_text = self.font.render('STATYSTYKI:', True, (0, 0, 0))
+        self.screen.blit(statistics_text, (self.width - 380, 250))
+
+        food_text = self.font.render('Zebrany nektar: ' + str(round(hive.nectar_stored, 3)) + ' g', True, (0, 0, 0))
+        self.screen.blit(food_text, (self.width - 380, 300))
+
+        food_text = self.font.render('Prognoza zbiorów na dziś: ' + str(round(hive.nectar_goal, 3)) + ' g', True, (0, 0, 0))
+        self.screen.blit(food_text, (self.width - 380, 350))
+
+        bee_text = self.font.render('Pszczoły wylatujące z ula: ' + str(len(hive.bees)), True, (0, 0, 0))
+        self.screen.blit(bee_text, (self.width - 380, 400))
+
+        bee_text = self.font.render('Umarłe pszczoły: ' + str(hive.dead_bees), True, (0, 0, 0))
+        self.screen.blit(bee_text, (self.width - 380, 450))
+
+        # ustawienia użytkownika
+        # TODO
+
+        # legenda
+        legend_text = self.font.render('LEGENDA:', True, (0, 0, 0))
+        self.screen.blit(legend_text, (self.width - 380, 680))
+
+        scaled_hive_img = pygame.transform.scale(self.hive_img, (40, 40))
+        hive_rect = scaled_hive_img.get_rect(midleft=(self.width - 380, 750))
+        self.screen.blit(scaled_hive_img, hive_rect)
+        legend_hive_text = self.font.render('- ul', True, (0, 0, 0))
+        legend_hive_text_rect = legend_hive_text.get_rect(midleft=(self.width - 320, 750))
+        self.screen.blit(legend_hive_text, legend_hive_text_rect)
+
+        food_source_full_rect = self.bush_flower_img.get_rect(center=(self.width - 360, 800))
+        self.screen.blit(self.bush_flower_img, food_source_full_rect)
+        legend_food_source_full_text = self.font.render('- pełne źródło jedzenia', True, (0, 0, 0))
+        legend_food_source_full_text_rect = legend_food_source_full_text.get_rect(midleft=(self.width - 320, 800))
+        self.screen.blit(legend_food_source_full_text, legend_food_source_full_text_rect)
+
+        food_source_empty_rect = self.bush_img.get_rect(center=(self.width - 360, 850))
+        self.screen.blit(self.bush_img, food_source_empty_rect)
+        legend_food_source_empty_text = self.font.render('- puste źródło jedzenia', True, (0, 0, 0))
+        legend_food_source_empty_text_rect = legend_food_source_empty_text.get_rect(midleft=(self.width - 320, 850))
+        self.screen.blit(legend_food_source_empty_text, legend_food_source_empty_text_rect)
+
+        bee_scout_rect = self.bee_img_orange.get_rect(center=(self.width - 360, 900))
+        self.screen.blit(self.bee_img_orange, bee_scout_rect)
+        legend_bee_scout_text = self.font.render('- pszczoła zwiadowczyni', True, (0, 0, 0))
+        legend_bee_scout_text_rect = legend_bee_scout_text.get_rect(midleft=(self.width - 320, 900))
+        self.screen.blit(legend_bee_scout_text, legend_bee_scout_text_rect)
+
+        bee_harvesting_rect = self.bee_img_blue.get_rect(center=(self.width - 360, 950))
+        self.screen.blit(self.bee_img_blue, bee_harvesting_rect)
+        legend_bee_harvesting_text = self.font.render('- pszczoła zbieraczka', True, (0, 0, 0))
+        legend_bee_harvesting_text_rect = legend_bee_harvesting_text.get_rect(midleft=(self.width - 320, 950))
+        self.screen.blit(legend_bee_harvesting_text, legend_bee_harvesting_text_rect)
